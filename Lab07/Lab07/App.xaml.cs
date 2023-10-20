4189c83edfce6504ba279a29450dd2601a02ca8e
LuisCorrales <luis.corrales@tecsup.edu.pe>
2023-10-20 15:23:29 -0500

    Botones Nav

--- Lab07/Lab07/App.xaml.cs
+++ Lab07/Lab07/App.xaml.cs
@@ -10,7 +10,7 @@ namespace Lab07
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- Lab07/Lab07/MainPage.xaml.cs
+++ Lab07/Lab07/MainPage.xaml.cs
@@ -13,6 +13,34 @@ namespace Lab07
         public MainPage()
         {
             InitializeComponent();
+            Item1.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitDemo());
+            };
+            Item2.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ExplicitControlDemo());
+            };
+            Item3.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitDemo());
+            };
+            Item4.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new ImplicitControlDemo());
+            };
+            Item5.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new GlobalDemo());
+            };
+            Item6.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new InheritanceDemo());
+            };
+            Item7.Clicked += async (sender, e) =>
+            {
+                await Navigation.PushAsync(new DynamicDemo());
+            };
         }
     }
 }
